f8665c8017384249865bf317a227eed7ed733c6a
Mightrader <clery.dav@gmail.com>
2026-02-19 11:40:56 +0100

    Fix tests with H2 profile

--- person-api/src/test/java/fr/afpa/api/personapi/PersonApiApplicationTests.java
+++ person-api/src/test/java/fr/afpa/api/personapi/PersonApiApplicationTests.java
@@ -2,8 +2,10 @@ package fr.afpa.api.personapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonApiApplicationTests {
 
     @Test
